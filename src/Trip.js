3620e866b55ffbc199d854e8abca48ae53efe8a3
bhohnco <bryanhohn13@gmail.com>
2021-04-22 11:38:39 -0600

    Generate cost of total trips method added

--- src/Trip.js
+++ src/Trip.js
@@ -1,12 +1,23 @@
 class Trip {
   constructor(trip) {
-    if (trip) {
-      this.id = trip.id || null;
-      this.userID = trip.userID || null;
-      this.destinationID = trip.destinationID || null;
-      this.travelers = trip.travelers || null;
-      this.date = trip.date || '0000/00/00';
-    }
+    this.id = trip.id || null;
+    this.userID = trip.userID || null;
+    this.destinationID = trip.destinationID || null;
+    this.travelers = trip.travelers || null;
+    this.date = trip.date || '0000/00/00';
+    this.duration = trip.duration || null;
+    this.status = trip.status || null;
+    this.suggestedActivities = trip.suggestedActivities || [];
+    this.cost = 0;
+  }
+
+  generateCostOfTravelersTrips(destination) {
+    let totalCost = 0;
+    const totalFlightCost = this.travelers * destination.estimatedFlightCostPerPerson;
+    const totalLodgingCost = this.duration * destination.estimatedLodgingCostPerDay;
+    totalCost += totalFlightCost;
+    totalCost += totalLodgingCost;
+    this.cost = totalCost;
   }
 }
 
