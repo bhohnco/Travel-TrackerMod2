0fff3a3dc5be919ea33726beb21d743abfe64982
bhohnco <bryanhohn13@gmail.com>
2021-04-22 12:59:34 -0600

    Test passing and all methods created

--- test/Traveler-Test.js
+++ test/Traveler-Test.js
@@ -10,7 +10,7 @@ const destinationTestingData = sampleTestingData.destinationTestingData;
 
 describe('Traveler', function () {
   let traveler1, traveler2, traveler3, trip1, trip2, trip3, trip4,
-    destination1, destination2, tripsTestGroup, destinationsTestGroup;
+    destination1, destination2, destination3, tripsTestGroup, destinationsTestGroup;
 
   beforeEach(() => {
     traveler1 = new Traveler(travelerTestingData[0]);
@@ -22,8 +22,9 @@ describe('Traveler', function () {
     trip4 = new Trip(tripTestingData[3]);
     destination1 = new Destination(destinationTestingData[0]);
     destination2 = new Destination(destinationTestingData[1]);
-    tripsTestGroup = [trip1, trip2, trip3];
-    destinationsTestGroup = [destination1, destination2];
+    destination3 = new Destination(destinationTestingData[2]);
+    tripsTestGroup = [trip1, trip3, trip4];
+    destinationsTestGroup = [destination1, destination2, destination3];
   });
 
   it('should be a function', () => {
@@ -40,6 +41,10 @@ describe('Traveler', function () {
 
   it('should have an name', () => {
     expect(traveler2.name).to.equal("Rachael Vaughten");
+  });
+
+  it('should have an traveler type', () => {
+    expect(traveler3.travelerType).to.equal("shopper");
   });
 
   it('if Traveler id property does not exist, it should set the default to null', () => {
@@ -86,11 +91,38 @@ describe('Traveler', function () {
     it ('should have a method that returns just the first name', () => {
       expect(traveler1.generateFirstName()).to.equal("Ham");
       expect(traveler2.generateFirstName()).to.equal("Rachael");
-    })
+    });
 
-    it('should be able to generate total trips for a user', () => {
+    it('should be able to generate array of trips given the array property name', () => {
+      traveler1.findTripInfo('currentTrips', trip1);
+      expect(traveler1.currentTrips).to.deep.equal([trip1]);
 
-    })
+      traveler3.findTripInfo('upcomingTrips', trip3);
+      expect(traveler3.upcomingTrips).to.deep.equal([trip3])
+
+      traveler2.findTripInfo('pendingTrips', trip2);
+      expect(traveler2.pendingTrips).to.deep.equal([trip2])
+
+      traveler2.findTripInfo('pastTrips', trip3);
+      expect(traveler2.pastTrips).to.deep.equal([trip3])
+    });
+
+    it ('should filter a travelers trips by year', () => {
+      expect(traveler2.generateTripsByYear(2020, tripsTestGroup)).to.deep.equal([ trip3, trip4]);
+      expect(traveler1.generateTripsByYear(2019, tripsTestGroup)).to.deep.equal([trip1]);
+    });
+
+    it ('should generate total trip cost per year for travelers and the duration of the trip', () => {
+      const tripGroup = traveler1.generateTripsByYear(2020, tripsTestGroup);
+      trip1.generateCostOfTravelersTrips(destination1);
+      trip3.generateCostOfTravelersTrips(destination3);
+      expect(traveler1.generateTripCost(tripGroup, tripsTestGroup)).to.equal(6010);
+    });
+
+    it ('should generate the 10% agent fee for a trip', () => {
+      expect(traveler1.generateAgentFees(6010)).to.equal(601)
+    });
+
   });
 
 });
