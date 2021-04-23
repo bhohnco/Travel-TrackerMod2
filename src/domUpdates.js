8d85783a3c210553ce364466f923253bee30f558
bhohnco <bryanhohn13@gmail.com>
2021-04-22 19:26:06 -0600

    Greet user and error function add

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,10 +1,35 @@
 const domUpdates = {
-  today:null,
+  today: null,
 
   toggleView(viewToDisplay, viewToHide) {
     viewToDisplay.classList.remove('hidden');
     viewToHide.classList.add('hidden');
   },
+
+  displayFormError(message) {
+    let errorMsg = document.getElementById('error-msg')
+    if (message === 'success') {
+      errorMsg.innerText = 'Username or password invalid. Please try again.';
+    } else {
+      errorMsg.innerText = '';
+    }
+  },
+
+  setDateDefaults() {
+    const searchBar = document.getElementById('traveler-date-search');
+    searchBar.value = this.today.replace(/\//g, "-");
+    console.log(this.today)
+  },
+
+  greetTraveler(traveler) {
+    let travelerDisplayName = document.getElementById('traveler-name');
+    travelerDisplayName.innerText = `Welcome back ${traveler.generateFirstName()}!`
+  },
+
+  displayTravelersTotalSpent(total) {
+    let totalSpent = document.getElementById("total-spent-traveler")
+  }
+
 }
 
 export default domUpdates;
