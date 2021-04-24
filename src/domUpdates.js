aafb28e6630935aec4596dec981f331703d1ede7
bhohnco <bryanhohn13@gmail.com>
2021-04-24 15:41:48 -0600

    Display past trip card function add

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,10 +1,9 @@
 const domUpdates = {
 
-  // toggleView(viewToDisplay, viewToHide) {
-  //   console.log("toggle")
-  //   viewToDisplay.classList.remove('hidden');
-  //   viewToHide.classList.add('hidden');
-  // },
+  toggleView(viewToDisplay, viewToHide) {
+    viewToDisplay.classList.remove('hidden');
+    viewToHide.classList.add('hidden');
+  },
 
   displayFormError(message) {
     let errorMsg = document.getElementById('error-msg')
@@ -15,24 +14,15 @@ const domUpdates = {
     }
   },
 
-  generateCurrentDate() {
-    const rawDate = new Date();
-    let day = rawDate.getDate();
-    if (day < 10) {
-      day = `0${day.toString()}`
-    }
-    let month = rawDate.getMonth() + 1;
-    if (month < 10) {
-      month = `0${month.toString()}`
-    }
-    const year = rawDate.getFullYear();
-    return `${year}/${month}/${day}`
+  getTodaysDate() {
+    let todaysDate = document.querySelector(".todays-date");
+    let date = new Date().toLocaleDateString("en-US").split("/");
+    todaysDate.innerText = `Today's Date: ${date[0]}/${date[1]}/${date[2]}`;
   },
 
   setDateDefaults() {
     const searchBar = document.getElementById('traveler-date-search');
     searchBar.value = this.today.replace(/\//g, "-");
-    console.log(this.today)
   },
 
   greetTraveler(traveler) {
@@ -40,12 +30,43 @@ const domUpdates = {
     travelerDisplayName.innerText = `Welcome back ${traveler.generateFirstName()}!`
   },
 
-
   displayTravelersTotalSpent(total) {
     let totalSpent = document.getElementById("total-spent-traveler");
     totalSpent.innerText = `You have spent $${total} in 2020`;
-  }
+  },
 
+  displayPastTrips(traveler, destinations) {
+    let past = document.querySelector(".past-trips-container");
+    past.innerHTML = "";
+    if (traveler.pastTrips[0] === undefined) {
+      past.innerHTML = `<p>You don't have any past trips, fill out the form above to plan your next trip!!</p>`
+    } else {
+      traveler.pastTrips.forEach(trip => {
+        let foundDest = findDestinationBasedOnTrip(trip, destinations);
+        let splitDestName = foundDest.destination.split(", ");
+        let dateSplit = trip.date.split("/");
+        past.innerHTML += `<section class="grid-container">
+     <article class="grid">
+      <section class="card top-row">
+        <img src="${foundDest.image}" alt="${foundDest.alt}">
+      </section>
+      <section class="lower-card">
+        <div class="card-text">
+          <h4 class="trip-destination">${splitDestName[0]},<br> ${splitDestName[1]}</h4>
+          <p class="date">Trip start date: ${dateSplit[1]}/${dateSplit[2]}/${dateSplit[0]}</p>
+        </div>
+        <p class="Duration">Duration of of trip: ${trip.duration}</p>
+        <p class="Number"> Number of travelers: ${trip.travelers}</p>
+      </section>
+    </article>`
+      })
+    }
+  }
 }
+
+function findDestinationBasedOnTrip(trip, destinations) {
+  return destinations.find(destination => destination.id === trip.destinationID);
+}
+  
 
 export default domUpdates;
