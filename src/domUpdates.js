425e9948517d2a99a7135338ec8f9d9116c619f5
bhohnco <bryanhohn13@gmail.com>
2021-04-25 08:21:27 -0600

    Display new trip cost added

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -47,6 +47,18 @@ const domUpdates = {
     })
   },
 
+  displayNewTripCost(cost, inputs) {
+    let newTripCost = document.querySelector(".new-trip-cost");
+    newTripCost.classList.remove("hidden")
+    if (inputs[0].value !== "" && inputs[3].value !== "0") {
+      newTripCost.innerHTML = `<p> This trip will cost $${cost} (including the agent fee) <p>`;
+      document.querySelector(".submit-request").disabled = false;
+    } else if (inputs[0].value === "") {
+      newTripCost.innerHTML = `<p>Please select a date!</p>`;
+    } else if (inputs[3].value === "0") {
+      newTripCost.innerHTML = `<p>Please select a destination!</p>`;
+    }
+  },
 
   displayPastTrips(traveler, destinations) {
     let past = document.querySelector(".past-trips-container");
@@ -104,33 +116,33 @@ const domUpdates = {
   //   }
   // },
 
-  // displayPendingTrips(traveler, destinations) {
-  //   let pending = document.querySelector(".pending-trips-container");
-  //   pending.innerHTML = "";
-  //   if (traveler.pendingTrips[0] === undefined) {
-  //     pending.innerHTML = `<p>You don't have any pending trips, start planning your next trip now!</p>`
-  //   } else {
-  //     traveler.pendingTrips.forEach(trip => {
-  //       let foundDest = findDestinationBasedOnTrip(trip, destinations);
-  //       let splitDestName = foundDest.destination.split(", ");
-  //       let dateSplit = trip.date.split("/");
-  //       pending.innerHTML += `<section class="grid-container">
-  //  <article class="grid">
-  //   <section class="card top-row">
-  //     <img src="${foundDest.image}" alt="${foundDest.alt}">
-  //   </section>
-  //   <section class="lower-card">
-  //     <div class="card-text">
-  //       <h4 class="trip-destination">${splitDestName[0]},<br> ${splitDestName[1]}</h4>
-  //       <p class="date">Trip start date: ${dateSplit[1]}/${dateSplit[2]}/${dateSplit[0]}</p>
-  //     </div>
-  //     <p class="Duration">Duration of of trip: ${trip.duration}</p>
-  //     <p class="Number"> Number of travelers: ${trip.travelers}</p>
-  //   </section>
-  // </article>`
-  //     })
-  //   }
-  // },
+  displayPendingTrips(traveler, destinations) {
+    let pending = document.querySelector(".pending-trips-container");
+    pending.innerHTML = "";
+    if (traveler.pendingTrips[0] === undefined) {
+      pending.innerHTML = `<p>You don't have any pending trips, start planning your next trip now!</p>`
+    } else {
+      traveler.pendingTrips.forEach(trip => {
+        let foundDest = findDestinationBasedOnTrip(trip, destinations);
+        let splitDestName = foundDest.destination.split(", ");
+        let dateSplit = trip.date.split("/");
+        pending.innerHTML += `<section class="grid-container">
+   <article class="grid">
+    <section class="card top-row">
+      <img src="${foundDest.image}" alt="${foundDest.alt}">
+    </section>
+    <section class="lower-card">
+      <div class="card-text">
+        <h4 class="trip-destination">${splitDestName[0]},<br> ${splitDestName[1]}</h4>
+        <p class="date">Trip start date: ${dateSplit[1]}/${dateSplit[2]}/${dateSplit[0]}</p>
+      </div>
+      <p class="Duration">Duration of of trip: ${trip.duration}</p>
+      <p class="Number"> Number of travelers: ${trip.travelers}</p>
+    </section>
+  </article>`
+      })
+    }
+  },
 
   //   displayCurrentTrips(traveler, destinations) {
   //     let current = document.querySelector(".current-trips-container");
