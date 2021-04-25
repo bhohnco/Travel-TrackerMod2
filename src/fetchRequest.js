e61bcf3339c49cd96bbc51ee4fc3907f9a8312e2
bhohnco <bryanhohn13@gmail.com>
2021-04-24 20:16:20 -0600

    Add traveler trip post/fetch

--- src/fetchRequest.js
+++ src/fetchRequest.js
@@ -1,4 +1,5 @@
 let fetchData = {
+
   generateSingleTraveler(travelerId) {
     return fetch(`http://localhost:3001/api/v1/travelers/${travelerId}`)
       .then(response => response.json())
@@ -30,7 +31,30 @@ let fetchData = {
         console.log(err, "Data error for all traveler fetch")
       })
   },
-      
+
+  generateNewTripForTraveler(trip) {
+    return fetch("http://localhost:3001/api/v1/trips", {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        id: trip.id,
+        userID: trip.userID,
+        destinationID: trip.destinationID,
+        travelers: trip.travelers,
+        date: trip.date,
+        duration: trip.duration,
+        status: trip.status,
+        suggestedActivities: trip.suggestedActivities
+      })
+    })
+      .then(response => response.json())
+      .catch(err => {
+        console.log(err, "Sorry! We are having trouble getting the data, try again later!")
+      })
+  }
 }
+
 
 export default fetchData;
